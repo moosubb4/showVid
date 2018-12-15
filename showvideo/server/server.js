bde93fd6828141cebedaf0b6252afa1cb46ef6cb
Mo0 Th4n4th0rn <thanathorn@vertice-international.com>
2018-12-15 21:49:07 +0700

    add can search and render function

--- showvideo/server/server.js
+++ showvideo/server/server.js
@@ -2,10 +2,13 @@ const vid = require('./db/vid.json');
 const axios = require('axios');
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express()
 const port = 8080
 const hostname = '127.0.0.1';
+
+const vidListData = '';
 
 const instance = axios.create({
     baseURL: 'https://s3-ap-southeast-1.amazonaws.com/ysetter/media/video-search.json',
@@ -22,14 +25,26 @@ const instance = axios.create({
 const getVideoList = async () => {
     try {
         const response = await instance.get();
-        const data = JSON.stringify(response.data);
-        // console.log(data);
-        return response.data;
+        const data = response.data;
+        return data;
     } catch (error) {
         console.error(error);
     }
 }
 
+const searchKey = async (key) => {
+    try {
+        const data = await getVideoList();
+        const item = data.items.filter((e) => e.snippet.title.indexOf(key) > -1);
+        // console.log("​searchKey -> item", item);
+        return item;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use(function (req, res, next) {
@@ -44,6 +59,19 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/discovery', async (req, res) => {
     try {
         const data = await getVideoList();
+        res.json(data);
+    } catch (e) {
+        next(e)
+    }
+});
+
+app.post('/search', async (req, res) => {
+
+    const keyWord = req.body.keyWord;
+
+    try {
+        const data = await searchKey(keyWord);
+        console.log("​data", data)
         res.json(data);
     } catch (e) {
         next(e)
