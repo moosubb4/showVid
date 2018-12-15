9f636ce0285bd58ca52d54ccde886575ae376736
Mo0 Th4n4th0rn <thanathorn@vertice-international.com>
2018-12-15 17:16:59 +0700

    aadd style

--- showvideo/src/service.js
+++ showvideo/src/service.js
@@ -1,28 +1,54 @@
-$().ready(function () {
+function kuy() {
+    alert('YOLO');
+}
+// const url = 'http://127.0.0.1:8080/';
 
-    const url = 'http://127.0.0.1:8080/';
+// async function getDiscovery() {
+//     try {
+//         const response = await axios.get(url + 'discovery');
+//         const data = response.data.items;
 
-    // async function getDiscovery() {
-    //     const path = url + 'discovery';
-    //     try {
-    //         const response = await axios.get(path);
-    //         console.log(response);
-    //     } catch (error) {
-    //         alert('oops');
-    //         console.error(error);
-    //     }
-    // }
+//         $.each(data, function (key, value) {
 
-    function getDiscovery() {
-        const path = url + 'discovery';
-        axios.get(path)
-            .then((result) => {
-                console.log("​result", result);
-            }).catch((err) => {
-                console.log("error", err);
-            });
-    }
+//             const {
+//                 etag,
+//                 id,
+//                 kind,
+//                 snippet
+//             } = value;
 
-    getDiscovery();
+//             // const title = snippet.title;
+//             const thumbnails = snippet.thumbnails["high"];
 
-});
+//             $('#app').append(`
+//                 <div class="item">                      
+//                   <!--<div class="videoWrapper">                                             
+//                      <iframe width="560" height="315" 
+//                      src="https://www.youtube.com/embed/${id.videoId}" 
+//                      frameborder="0" allow="accelerometer; autoplay; 
+//                      encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//                   </div>-->
+//                   <!-- <div class="thumbnails">
+//                     <img src="${thumbnails.url}" style="height:${thumbnails.height};width:${thumbnails.width}"></img>
+//                   </div>-->
+//                   <p>${snippet.title}</p>
+//                   <a href="https://www.youtube.com/channel/${snippet.channelId}">
+//                     ${snippet.channelTitle}
+//                   </a>
+//                   <p>${snippet.description}</p>
+//                 </div>
+//                 `);
+
+
+//         });
+
+//         console.log(data);
+//     } catch (error) {
+//         alert('oops');
+//         console.error(error);
+//     }
+// }
+// $().ready(function () {
+//     alert('YOLO');
+// });
+
